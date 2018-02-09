b8239baf89c66f29629820de7e258b24f96470b3
Thomas Osterbind <tom@qri.io>
2018-02-09 13:50:33 -0500

    test: updated test output to match change from 'qri' to 'kind'

--- datasetDiffer_test.go
+++ datasetDiffer_test.go
@@ -73,7 +73,7 @@ func TestDiffDataset(t *testing.T) {
 		{"testdata/orig.json", "testdata/newVisConfig.json", "plusMinus", ` {
 -  "format": "abc",
 +  "format": "new thing",
-   "qri": "vc:0"
+   "kind": "vc:0"
  }
 `, ""},
 		{"testdata/orig.json", "testdata/newTransform.json", "simple", "Transform Changed. (4 changes)", ""},
